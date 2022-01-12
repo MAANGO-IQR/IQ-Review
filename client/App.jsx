d1c8c07498220acecec96d5c5786be45d9c10e37
Cristian De Los Rios <Cris2595@gmail.com>
2022-01-12 09:41:15 -0700

    moved navbar to app

--- client/App.jsx
+++ client/App.jsx
@@ -3,16 +3,21 @@ import { Routes, Route } from 'react-router';
 import CreateQuestionPage from './pages/CreateQuestionPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-
+import HomePage from './pages/HomePage';
+import AboutUs from './pages/AboutUs';
+import Navbar from './components/login/NavBar';
 function App() {
   return (
+    <>
+    <Navbar></Navbar>
       <Routes>
-        {/* <Route path='/aboutus' element={<AboutUs />} /> */}
+        <Route path='/aboutus' element={<AboutUs />} /> 
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
         <Route path='/iq' element={<CreateQuestionPage />} />
-        {/* <Route path='/' element={<HomePage />} />  */}
+        <Route path='/' element={<HomePage />} />  
       </Routes>
+      </>
     );
 }
 export default App;
