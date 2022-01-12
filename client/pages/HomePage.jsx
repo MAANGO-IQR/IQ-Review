72f01b3328a80a03ae63631c88d083b1c2f0daa0
Cristian De Los Rios <Cris2595@gmail.com>
2022-01-12 11:28:44 -0700

    added react context.js

--- client/pages/HomePage.jsx
+++ client/pages/HomePage.jsx
@@ -1,22 +1,20 @@
 //make homePage.jsx
- import React from 'react';
- import MangoSelect from '../components/login/maangoSelect';
- import DifficultySelect from '../components/login/difficultySelect';
-import CategorySelect from '../components/login/categorySelect';
+import React from "react";
+import MangoSelect from "../components/login/maangoSelect";
+import DifficultySelect from "../components/login/difficultySelect";
+import CategorySelect from "../components/login/categorySelect";
+import Feed from "../components/feed/Feed";
 
-
-       
-
-        function HomePage(){
-            return(
-            <>
-           
-            <div className='homeContainer'>
-               <MangoSelect></MangoSelect>
-               <DifficultySelect></DifficultySelect>
-               <CategorySelect></CategorySelect>
-            </div>
-            </>
-            )
-        }
-        export default HomePage;
+function HomePage() {
+  return (
+    <>
+      <div className="homeContainer">
+        <MangoSelect></MangoSelect>
+        <DifficultySelect></DifficultySelect>
+        <CategorySelect></CategorySelect>
+        <Feed></Feed>
+      </div>
+    </>
+  );
+}
+export default HomePage;
